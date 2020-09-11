5ffc27dc9d6f1a2375d098ff2d3a35d13ef73546
sona <sonuvpatel94@gmail.com>
2020-09-11 13:24:50 -0500

    Add project files.

---
+++ Models/BlindDatingContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlindDating.Models
+{
+    public partial class BlindDatingContext : DbContext
+    {
+        public BlindDatingContext()
+        {
+        }
+
+        public BlindDatingContext(DbContextOptions<BlindDatingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DatingProfile> DatingProfile { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Database = BlindDating;Trusted_Connection= True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DatingProfile>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Bio)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserAccountId)
+                    .IsRequired()
+                    .HasColumnName("UserAccountID")
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
